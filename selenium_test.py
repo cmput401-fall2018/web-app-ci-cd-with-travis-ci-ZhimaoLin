1bf659404ef512f89b2fc5ab2b0706511e5720c1
Zhimao Lin <zhimao@ualberta.ca>
2018-10-24 10:14:48 -0600

    ADD TEST

--- selenium_test.py
+++ selenium_test.py
@@ -1,2 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 def test_home():
-    assert True
+    driver = webdriver.Chrome()
+    driver.get("http://162.246.157.223:8000")
+    elem = driver.find_element_by_id("name")
+    assert elem != None
