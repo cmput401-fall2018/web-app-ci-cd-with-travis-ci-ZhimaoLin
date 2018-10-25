a1acd95561100904c513e95b496a22351c44545f
Zhimao Lin <zhimao@ualberta.ca>
2018-10-24 23:53:30 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 
 def test_home():
     chrome_options = Options()
-    chrome_options.binary_location = "./chromedriver"
+    chrome_options.binary_location = "web-app-ci-cd-with-travis-ci-ZhimaoLin/chromedriver"
 
     driver = webdriver.Chrome()
 
