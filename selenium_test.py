f2f35797c080bdf6fbe7e0027bb8179b41f72092
Zhimao Lin <zhimao@ualberta.ca>
2018-10-24 23:28:21 -0600

    try this way

--- selenium_test.py
+++ selenium_test.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 def test_home():
+    chrome_options = Options()
+    chrome_options.binary_location("./chromedriver")
 
     driver = webdriver.Chrome()
 
