30958f7e2ef7bb9840588e2859072720048565f8
Zhimao Lin <zhimao@ualberta.ca>
2018-10-25 00:33:31 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -11,6 +11,23 @@ def test_home():
     driver.get("http://162.246.157.223:8000")
     elem = driver.find_element_by_id("name")
     assert elem.text == "Zhimao Lin"
-    # assert elem != None
 
+    elem = driver.find_element_by_id("about")
+    assert elem.text == "I am a computing science student at u of a."
+
+    elem = driver.find_element_by_id("education")
+    assert elem.text == "University of Alberta"
+
+    elem = driver.find_element_by_id("skills")
+    assert elem.text == "Programming"
+
+    elem = driver.find_element_by_id("work")
+    assert elem.text == "Student"
+
+    elem = driver.find_element_by_id("contact")
+    assert elem.text == "zhimao@ualberta.ca"
+
+
+    # assert elem != None
+    # I am a computing science student at u of a.
     assert True
