1b193a4ea41b522154e62ccbcae8279218d09376
Zhimao Lin <zhimao@ualberta.ca>
2018-10-25 00:21:00 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -8,4 +8,9 @@ def test_home():
 
     driver = webdriver.Chrome()
 
+    driver.get("http://162.246.157.223:8000")
+    elem = driver.find_element_by_id("name")
+    print(elem.text)
+    # assert elem != None
+
     assert True
