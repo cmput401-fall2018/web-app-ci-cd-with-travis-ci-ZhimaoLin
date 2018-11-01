e0ce02497c6eb48553e27f5ddc55afbf5c2464de
Zhimao Lin <zhimao@ualberta.ca>
2018-11-01 13:30:01 -0600

    Update test_service.py

--- test_service.py
+++ test_service.py
@@ -13,13 +13,28 @@ def test_bad_random(bad_random):
 def test_divide(bad_random):
     service = Service()
 
-    # Test 0 / x
+    # Test 0 / x, such that x > 0
     bad_random.return_value = 0
     assert service.divide(5) == 0
 
-    # Test y / x such that y > 0
+    # Test 0 / x, such that x < 0
+    bad_random.return_value = 0
+    assert service.divide(-5) == 0
+
+    # Test y / x such that y > 0 and x > 0
     bad_random.return_value = 6
     assert service.divide(3) == 2
+
+    # Test y / x such that y > 0 and x < 0
+    bad_random.return_value = 6
+    assert service.divide(-3) == -2
+
+    # Test y / x such that x = 0
+    bad_random.return_value = 6
+    try:
+        service.divide(0)
+    except ZeroDivisionError:
+        assert True
 
 
 def test_abs_plus():
@@ -40,12 +55,42 @@ def test_abs_plus():
 def test_complicated_function(divide, bad_random):
     service = Service()
 
+    # divide > 0 and bad_random = 0
+    divide.return_value = 5
+    bad_random.return_value = 0
+    result = service.complicated_function(6)
+    assert result[0] == 5
+    assert result[1] == 0
+
+    # divide = 0 and bad_random = 0
     divide.return_value = 0
     bad_random.return_value = 0
     result = service.complicated_function(6)
     assert result[0] == 0
     assert result[1] == 0
 
+    # divide < 0 and bad_random = 0
+    divide.return_value = -6
+    bad_random.return_value = 0
+    result = service.complicated_function(6)
+    assert result[0] == -6
+    assert result[1] == 0
+
+    # divide < 0 and bad_random > 0
+    divide.return_value = -6
+    bad_random.return_value = 7
+    result = service.complicated_function(6)
+    assert result[0] == -6
+    assert result[1] == 7 % 2
+
+    # divide = 0 and bad_random > 0
+    divide.return_value = 0
+    bad_random.return_value = 7
+    result = service.complicated_function(6)
+    assert result[0] == 0
+    assert result[1] == 7 % 2
+
+    # divide > 0 and bad_random > 0
     divide.return_value = 6
     bad_random.return_value = 7
     result = service.complicated_function(6)
